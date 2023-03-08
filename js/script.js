af2e1d4419fc0c14fe22fd6c774c7740a60b2db2
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-03-08 18:18:46 +0100

    applicazione sconti e comparsa a schermo

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 // Chiedo all'utente il numero di km che vuole percorrere
-const UserKm = parseInt(prompt("Quanti anni km vuole percorrere?"));
+const UserKm = parseInt(prompt("Quanti km vuole percorrere?"));
 console.log(UserKm);
 // Chiedo all'utente la sua età
-const UserAge = parseInt(prompt("Quanti anni ha?"));
+const UserAge = parseInt(prompt("Quanti anni hai?"));
 console.log(UserAge);
 
 // Prezzo del biglietto definito in base ai km (0.21 € al km)
 const PriceKm = 0.21
-const FinalPrice = UserKm*PriceKm
+let FinalPrice = UserKm*PriceKm
 console.log(FinalPrice);
-// Applicazione sconto del 20% per i minorenni
 
+// Applicazione sconto del 20% per i minorenni
 // Applicazione sconto del 40% per gli over 65
+if(UserAge<18){
+    FinalPrice=FinalPrice*(1-0.20)
+}
+else if(UserAge>65){
+    FinalPrice=FinalPrice*(1-0.40)
+}
+console.log(FinalPrice);
+
+// Risultato a schermo
+
+document.getElementById("result").innerHTML = FinalPrice;
