c29d97a8ac54ad583945ccb829f879de7722f19c
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-03-08 15:20:24 +0100

    domande per l'utente

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Chiedo all'utente il numero di km che vuole percorrere
+const UserKm = parseInt(prompt("Quanti anni km vuole percorrere?"));
+console.log(UserKm);
+// Chiedo all'utente la sua età
+const UserAge = parseInt(prompt("Quanti anni ha?"));
+console.log(UserAge);
